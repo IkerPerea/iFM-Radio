e2c0f6c04acca82c1b31ca4b0e87318c0d074908
Iker Perea Trejo <ikerpereatrejo@gmail.com>
2024-03-02 20:03:46 +0100

    Initial Commit

---
+++ iFMRadio/iFMRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFMRadioApp.swift
+//  iFMRadio
+//
+//  Created by Iker Perea Trejo on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct iFMRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
